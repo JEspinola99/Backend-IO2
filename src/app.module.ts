54167ca4930910102cf4d8284545738f7a3168a3
Vania Velazquez <velazquezvania000@gmail.com>
2023-10-14 16:41:49 -0300

    Entidad Tarea y Etiqueta agregado

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { WorkSpaceModule } from './work-space/work-space.module';
 import { WorkSpaceUserModule } from './work-space-user/work-space-user.module';
 import { BoardsModule } from './boards/boards.module';
 import { ColumnModule } from './column/column.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ConfigModule.forRoot(), WorkSpaceModule, WorkSpaceUserModule, BoardsModule, ColumnModule],
+  imports: [AuthModule, UsersModule, ConfigModule.forRoot(), WorkSpaceModule, WorkSpaceUserModule, BoardsModule, ColumnModule, TasksModule],
   providers: []
 })
 export class AppModule {}
